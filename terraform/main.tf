be9fdf462bce4c3f62be3c1177977b8d79074550
Folayan Donald <donfolayan@gmail.com>
2024-08-06 22:44:56 +0100

    pulling my hair out

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,8 +96,8 @@ resource "azurerm_kubernetes_cluster" "capstone-aks" {
   }
 
   service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
+    client_id     = var.appId
+    client_secret = var.password
   }
 
   role_based_access_control_enabled = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,9 @@
-# variables.tf
-
-variable "client_id" {
-  description = "The client ID for the Azure Service Principal."
-  type        = string
-  sensitive   = true
+variable "appId" {
+  description = "Azure Kubernetes Service Cluster service principal"
+  type = string
 }
 
-variable "client_secret" {
-  description = "The client secret for the Azure Service Principal."
-  type        = string
-  sensitive   = true
-}
-
-variable "subscription_id" {
-  description = "The subscription ID for the Azure account."
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "The tenant ID for the Azure account."
-  type        = string
-  sensitive   = true
+variable "password" {
+  description = "Azure Kubernetes Service Cluster password"
+  type = string
 }
